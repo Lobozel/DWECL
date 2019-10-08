22354a000fa7b30367c782b2e68255eec189c423
progmiguel <55480620+progmiguel@users.noreply.github.com>
2019-10-08 13:03:33 +0200

    Ejercicio 3 y cambios pequeños

---
+++ UD2/Relacion1/Ejercicio3/js/codigo.js
@@ -0,0 +1,18 @@
+//Almaceno los 12 meses del año en un array
+var meses=[
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+];
+for(i=0;i<meses.length;i++){
+    alert(meses[i]);
+}
