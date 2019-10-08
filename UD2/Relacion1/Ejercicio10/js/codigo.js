ff31bb7671a4f02c6075d08d26d8148b98df09e6
Lobozel <33419952+Lobozel@users.noreply.github.com>
2019-10-08 18:00:51 +0200

    Ejercicio 10 terminado

---
+++ UD2/Relacion1/Ejercicio10/js/codigo.js
@@ -0,0 +1,15 @@
+let numero1 = 5;
+let numero2 = 8;
+if(numero1<numero2){
+    alert("numero1 no es mayor que numero2");
+}
+//En la aritmética cotidiana 0 no es ni positivo ni negativo.
+if(numero2>0){
+    alert("numero2 es positivo");
+}
+if(numero1<0){
+    alert("numero1 es negativo o distinto de cero");
+}
+if((numero1+1)<numero2){
+    alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
