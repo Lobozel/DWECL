a792d27e7d6ca7a844c8715f11d41a551f2fac21
Lobozel <33419952+Lobozel@users.noreply.github.com>
2019-10-09 11:23:56 +0200

    Ejercicio 9 terminado

--- UD2/Relacion1/Ejercicio9/js/codigo.js
+++ UD2/Relacion1/Ejercicio9/js/codigo.js
@@ -1,2 +1,26 @@
 let valores = [true,5,false,"hola","adios",2];
 
+//Comparo los dos elementos de texto y escribo por pantalla cual es mayor
+if(valores[3].length>valores[4].length){
+    document.write (valores[3]+" es más grande que "+valores[4]);
+}else{
+    document.write (valores[4]+" es más grande que "+valores[3]);
+}
+
+document.write("<br>");
+//Operación lógica con los valores booleanos que devuelve true
+document.write(valores[0]||valores[2]);
+document.write("<br>");
+//Operación lógica con los valores booleanos que devuelve false
+document.write(valores[0]&&valores[2]);
+document.write("<br>");
+//Escribo por pantalla las 5 operaciones matemáticas básicas con los dos elementos númericos
+document.write(valores[1]+"+"+valores[5]+"="+(parseInt(valores[1])+parseInt(valores[5])));
+document.write("<br>");
+document.write(valores[1]+"-"+valores[5]+"="+(parseInt(valores[1])-parseInt(valores[5])));
+document.write("<br>");
+document.write(valores[1]+"*"+valores[5]+"="+(parseInt(valores[1])*parseInt(valores[5])));
+document.write("<br>");
+document.write(valores[1]+"/"+valores[5]+"="+(parseInt(valores[1])/parseInt(valores[5])));
+document.write("<br>");
+document.write("modulo de "+valores[1]+" y "+valores[5]+"="+(parseInt(valores[1])%parseInt(valores[5])));
