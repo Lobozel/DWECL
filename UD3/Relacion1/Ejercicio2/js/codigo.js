0b0c9a3a40e20de0ccd887ab4125eb5824c828ad
progmiguel <55480620+progmiguel@users.noreply.github.com>
2019-10-16 09:01:14 +0200

    ya tengo un format xD

--- UD3/Relacion1/Ejercicio2/js/codigo.js
+++ UD3/Relacion1/Ejercicio2/js/codigo.js
@@ -9,70 +9,81 @@ let fecha2;
 let validacion;
 let dias;
 
-do{
-	fecha = prompt('Ingresa una primera fecha con el formato YYYY-MM-DD:');
+do {
+  fecha = prompt("Ingresa una primera fecha con el formato YYYY-MM-DD:");
 
-	validacion=validarFecha(fecha);
+  validacion = validarFecha(fecha);
 
-	if(!validacion){
-		alert("La fecha NO es válida");
-	}else{
-		do{
-            fecha2 = prompt('Ingresa una segunda con el formato YYYY-MM-DD:');
-        
-            validacion=validarFecha(fecha);
-        
-            if(!validacion){
-                alert("La segunda fecha NO es válida");
-            }else{
-                fecha=moment(fecha);
-                fecha2=moment(fecha2);
+  if (!validacion) {
+    alert("La fecha NO es válida");
+  } else {
+    do {
+      fecha2 = prompt("Ingresa una segunda con el formato YYYY-MM-DD:");
 
-                dias=fecha2.diff(fecha, 'days');
-                if (dias<0){
-                    dias=-dias;
-                }
+      validacion = validarFecha(fecha);
 
-		        document.write(dias, ' días de diferencia');
+      if (!validacion) {
+        alert("La segunda fecha NO es válida");
+      } else {
+        fecha = moment(fecha);
+        fecha2 = moment(fecha2);
 
-            }
-        }while(!validacion);
-	}
-}while(!validacion);
+        dias = fecha2.diff(fecha, "days");
+        if (dias < 0) {
+          dias = -dias;
+        }
 
-function validarFecha(fecha){
- 
-	if(fecha.length!=10 || fecha.charAt(4)!='-' || fecha.charAt(7)!='-' ||
-	parseInt(fecha.substring(5, 7))<1 || parseInt(fecha.substring(5, 7))>12 //comprobacion mes
-	|| parseInt(fecha.substring(8,10))<1 || parseInt(fecha.substring(8,10))>//comprobacion dias
-	diasMes(parseInt(fecha.substring(5, 7)),parseInt(fecha.substring(0, 4)))){
-		return false;
-	}
-	return true;
+        document.write(dias, " días de diferencia");
+      }
+    } while (!validacion);
+  }
+} while (!validacion);
+
+function validarFecha(fecha) {
+  if (
+    fecha.length != 10 ||
+    fecha.charAt(4) != "-" ||
+    fecha.charAt(7) != "-" ||
+    parseInt(fecha.substring(5, 7)) < 1 ||
+    parseInt(fecha.substring(5, 7)) > 12 || //comprobacion mes
+    parseInt(fecha.substring(8, 10)) < 1 ||
+    parseInt(fecha.substring(8, 10)) > //comprobacion dias
+      diasMes(parseInt(fecha.substring(5, 7)), parseInt(fecha.substring(0, 4)))
+  ) {
+    return false;
+  }
+  return true;
 }
 
-function diasMes(mes,anio){
-    let dias=0;
-    switch (mes){
-		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-			dias=31;
-			break;
-		case 4: case 6: case 9: case 11:
-			dias=30;
-			break;
-		case 2:
-			if(bisiesto(anio)) 
-				dias=29;
-			else
-				dias=28;
-		}
-		return dias;
+function diasMes(mes, anio) {
+  let dias = 0;
+  switch (mes) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      dias = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      dias = 30;
+      break;
+    case 2:
+      if (bisiesto(anio)) dias = 29;
+      else dias = 28;
+  }
+  return dias;
 }
 
-function bisiesto(anio){
-    let bisiesto=false;
-    if(anio%4==0 || anio%400==0 && anio%100!=0){
-        bisiesto=true;
-    }
-		return bisiesto;
+function bisiesto(anio) {
+  let bisiesto = false;
+  if (anio % 4 == 0 || (anio % 400 == 0 && anio % 100 != 0)) {
+    bisiesto = true;
+  }
+  return bisiesto;
 }
--- UD3/Relacion1/Ejercicio8/js/codigo.js
+++ UD3/Relacion1/Ejercicio8/js/codigo.js
@@ -5,20 +5,9 @@ Explica cada uno de los términos de la expresión.
 */
 
 let cadena = "Test 123123329";
-let expresion="[0-9]";
-
-let nuevaCadena="";
-let index=0;
-let aux=cadena;//Para no cargarme el valor de cadena
-
-do{
-    index=aux.search(expresion); //Busco la primera coincidencia del la expresion regular
-    nuevaCadena+=aux.charAt(index); //Añado esta a la nueva cadena
-    aux=aux.replace(aux.charAt(index),""); //elimino esta de la cadena vieja para poder buscar la siguiente coincidencia
-}while(index!=-1); //si devuelve -1 es que no hay mas coincidencias
+let expresion="[0-9]{9}"; //[0-9]{9} => Busca 9 dígitos consecutivos
 
 
 document.write(cadena+"<br>");
-document.write(nuevaCadena+"<br>");
-
-// document.write(cadena.search(expresion));
+//match nos devuelve un array donde el indice 0 es la subcadena sacada a partir de la expresión regular
+document.write((cadena.match(expresion))[0]);
