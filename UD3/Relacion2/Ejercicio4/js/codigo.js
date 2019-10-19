1277dbf4a092f4fbf70c6f891c88588ff3f89a77
Lobozel <33419952+Lobozel@users.noreply.github.com>
2019-10-19 17:55:23 +0200

    ejercicio 4 completado

--- UD3/Relacion2/Ejercicio4/js/codigo.js
+++ UD3/Relacion2/Ejercicio4/js/codigo.js
@@ -1,3 +1,26 @@
 /*
 Ejercicio 4 - Partiendo del mismo ejercicio anterior, añade un botón que compruebe si la ventana que se tiene que abrir, ha sido ya abierta, está abierta, o ha sido ya cerrada.
 */
+
+let ventana;
+
+function abrirVentana(){
+   ventana = window.open("","Ejercicio2","toolbar=yes, scrollbars=yes, resizable=no, width=400, height=400, left=500")
+}
+
+function cerrarVentana(){
+    ventana.close();
+}
+
+function comprobar(){
+    if(ventana==null){
+        document.getElementById("demo").innerHTML = "La ventana no se ha abierto";
+
+    }
+    if(ventana.closed){
+        document.getElementById("demo").innerHTML = "La ventana se ha cerrado";
+    }else{
+        document.getElementById("demo").innerHTML = "La ventana sigue abierta";
+    }
+    
+}
