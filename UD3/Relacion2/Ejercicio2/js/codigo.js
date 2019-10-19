0ca9e8bf3c09ee4426c54c24eb048ad8668b75c3
Lobozel <33419952+Lobozel@users.noreply.github.com>
2019-10-19 17:36:00 +0200

    Ejercicio2 resuelto

--- UD3/Relacion2/Ejercicio2/js/codigo.js
+++ UD3/Relacion2/Ejercicio2/js/codigo.js
@@ -1,3 +1,7 @@
 /*
 Ejercicio 2 - Abre un nueva ventana con los siguientes parámetros: que tenga barra de herramientas, que sea escrolable, no redimensionable, y con una anchura y altura de 400 pixeles. Los demás parámetros son indiferentes.
 */
+
+function abrirVentana(){
+    window.open("","Ejercicio2","toolbar=yes, scrollbars=yes, resizable=no, width=400, height=400, left=0, top=0")
+}
