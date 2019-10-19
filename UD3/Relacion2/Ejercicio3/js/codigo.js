2b59f63762a481c252a75749ded3ec59ce01e252
Lobozel <33419952+Lobozel@users.noreply.github.com>
2019-10-19 17:42:19 +0200

    Ejercicio 3 terminado

--- UD3/Relacion2/Ejercicio3/js/codigo.js
+++ UD3/Relacion2/Ejercicio3/js/codigo.js
@@ -1,3 +1,13 @@
 /*
 Ejercicio 3 - Haz una página en la aparecen 2 botones: uno abre una ventana y el segundo cierra la misma ventana.
 */
+
+let ventana;
+
+function abrirVentana(){
+   ventana = window.open("","Ejercicio2","toolbar=yes, scrollbars=yes, resizable=no, width=400, height=400, left=500")
+}
+
+function cerrarVentana(){
+    ventana.close();
+}
