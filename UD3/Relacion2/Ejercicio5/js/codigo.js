0bda6be433efc434a9ac90d79334b1fc4308a85f
Lobozel <33419952+Lobozel@users.noreply.github.com>
2019-10-19 18:17:08 +0200

    ejercicio 5 completado

--- UD3/Relacion2/Ejercicio5/js/codigo.js
+++ UD3/Relacion2/Ejercicio5/js/codigo.js
@@ -8,3 +8,39 @@ Posicionar en la posición (0, 0) la ventana.
 Desplazar la ventana hacia la derecha.
 Para cada una de estas funciones debemos de crear un botón, y mostrar por pantalla el tamaño de la ventana para poder hacer un seguimiento de los cambios de la misma.
 */
+
+let ventana;
+
+function abrirVentana(){
+   ventana = window.open("","Ejercicio2","toolbar=yes, scrollbars=yes, resizable=no, width=400, height=400, left=500")
+    document.getElementById("ancho").innerHTML = ventana.innerWidth;
+    document.getElementById("alto").innerHTML = ventana.innerHeight;
+}
+
+function cerrarVentana(){
+    ventana.close();
+    document.getElementById("ancho").innerHTML ="";
+    document.getElementById("alto").innerHTML = "";
+}
+
+function agrandarVentana(){
+    ventana.resizeBy(100,100);
+    //El +100 es porque tarda en actualizarse
+    document.getElementById("ancho").innerHTML = ventana.innerWidth+100;
+    document.getElementById("alto").innerHTML = ventana.innerHeight+100;
+}
+
+function disminuirVentana(){
+    ventana.resizeBy(-100,-100);
+    //El -100 es porque tarda en actualizarse
+    document.getElementById("ancho").innerHTML = ventana.innerWidth-100;
+    document.getElementById("alto").innerHTML = ventana.innerHeight-100;
+}
+
+function posicionarVentana(){
+    ventana.moveTo(0,0);
+}
+
+function desplazarVentana(){
+    ventana.moveBy(10, 0);
+}
