86d7584d7d44e66a7cdf8ed81e275173fad68d1b
progmiguel <55480620+progmiguel@users.noreply.github.com>
2019-11-05 14:31:07 +0100

    Update codigo.js

--- UD4/Map-Filter-Reduce/Ejercicio2/js/codigo.js
+++ UD4/Map-Filter-Reduce/Ejercicio2/js/codigo.js
@@ -1,37 +1,34 @@
 var pilots = [
-    {
-        id: 2,
-        name: "Wedge Antilles",
-        faction: "Rebels"
-    },
-    {
-        id: 8,
-        name: "Ciena Ree",
-        faction: "Empire"
-    },
-    {
-        id: 40,
-        name: "Iden Versio",
-        faction: "Empire"
-    },
-    {
-        id: 66,
-        name: "Thane Kyrell",
-        faction: "Rebels"
-    }
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels"
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire"
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire"
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels"
+  }
 ];
 
 let rebeldes = pilots.filter(function(pilots) {
-    return pilots.faction == "Rebels"
+  return pilots.faction == "Rebels";
 });
 
 console.log("Pilotos rebeldes sin arrow:");
 console.log(Object.values(rebeldes));
 
-
-rebeldes = pilots.filter(pilots=>pilots.faction=="Rebels");
+rebeldes = pilots.filter(pilots => pilots.faction == "Rebels");
 
 console.log("Pilotos rebeldes con arrow:");
 console.log(Object.values(rebeldes));
-
-
