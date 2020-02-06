05df6c658cd9bbe971faa040d2b1d4ac2c383c32
Lobozel <33419952+Lobozel@users.noreply.github.com>
2020-02-06 03:43:31 +0100

    formato

--- UD8/Ejercicio3/js/codigo.js
+++ UD8/Ejercicio3/js/codigo.js
@@ -1,16 +1,15 @@
 const lsName = "elementos";
-  let item = document.getElementById("item");
-  let lista = document.getElementsByTagName("ul")[0];
+let item = document.getElementById("item");
+let lista = document.getElementsByTagName("ul")[0];
 
 if (typeof Storage == "undefined") {
   alert("El navegador NO soporta WebStorage");
 } else {
-
-    mostrar();
+  mostrar();
 
   document.getElementById("miFormulario").onsubmit = function(e) {
     e.preventDefault();
-  };  
+  };
 
   document.getElementById("clear").addEventListener("click", function() {
     localStorage.clear();
@@ -20,20 +19,23 @@ if (typeof Storage == "undefined") {
   item.addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
       añadirElemento(lsName, item.value);
-      item.value="";
+      item.value = "";
     }
   });
 }
 
 function añadirElemento(lsName, valor) {
-    if(localStorage.getItem(lsName)!=null){
-        localStorage.setItem(lsName, localStorage.getItem(lsName)+"<li>"+valor+"</li>");
-    }else{
-        localStorage.setItem(lsName, "<li>"+valor+"</li>");
-    }    
-        mostrar();
+  if (localStorage.getItem(lsName) != null) {
+    localStorage.setItem(
+      lsName,
+      localStorage.getItem(lsName) + "<li>" + valor + "</li>"
+    );
+  } else {
+    localStorage.setItem(lsName, "<li>" + valor + "</li>");
+  }
+  mostrar();
 }
 
-function mostrar(){
-    lista.innerHTML=localStorage.getItem(lsName);    
+function mostrar() {
+  lista.innerHTML = localStorage.getItem(lsName);
 }
