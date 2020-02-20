cf3774a470f52a2d7290cb0c36f4e3b1cc12acee
progmiguel <55480620+progmiguel@users.noreply.github.com>
2020-02-20 13:00:44 +0100

    Ejercicio 3 listo

---
+++ Examenes/Examen3/Ejercicio3/js/chat.js
@@ -0,0 +1,71 @@
+window.onload = function(){
+    let texto1 = document.getElementById("texto1");
+    texto1.addEventListener("keypress",function(e) {
+        if (e.key === "Enter") {
+          add('user1',texto1);
+        }
+    });
+
+    document.getElementById("botonAnadir1")
+    .addEventListener('click',function(){
+        add('user1',texto1);
+    });
+
+    let texto2 = document.getElementById("texto2");
+    texto2.addEventListener("keypress",function(e) {
+        if (e.key === "Enter") {
+            add('user2',texto2);
+        }
+    });
+
+    document.getElementById("botonAnadir2")
+    .addEventListener('click',function(){
+        add('user21',texto2);
+    });
+
+    document.getElementById("botonLimpiar")
+    .addEventListener('click',reset);
+
+    //se ha usado el chat antes?
+    if(localStorage.getItem('chat')==null){
+        localStorage.setItem('chat',"");
+    }else{
+        this.write();
+    }
+
+}
+
+function add(user,text){
+    //existe?
+    if(localStorage.getItem(user)==null){
+        let value = prompt("Introduzca su nombre.");
+        localStorage.setItem(user,value);
+    }
+
+    let name = localStorage.getItem(user);
+
+    //ultimo mensaje
+    localStorage.setItem(name,text.value);
+
+    //chat
+    localStorage.setItem('chat',
+    localStorage.getItem('chat')+
+    name+": "+
+    localStorage.getItem(name)+"\n"
+    );
+
+    //escribir
+    this.write();
+
+    //limpiar
+    text.value="";
+}
+
+function write(){
+    document.getElementById('textarea').innerHTML=localStorage.getItem('chat');
+}
+
+function reset(){
+    localStorage.setItem('chat',"");
+    write();
+}
